3d2fbbb2e066d9238c8e7ff5a027a3d4a5ae5719
ayiekkk <mohamadalief547@gmail.com>
2026-01-25 20:40:12 +0700

    add

--- pages/home.php
+++ pages/home.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    <div class="container-home">
-        <div class="left-section">
-            <img src="../img/CroodsChart.png" alt="Chart Image">
-        </div>
-        <div class="right-section">
-            <h1 class="title">tata dompet</h1>
-            <p class="tagline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, dicta enim sunt doloremque repellat perferendis.</p>
-            <a href="main.php" class="cta-button">Let's Start</a>
-        </div>
+<?php require_once 'components/header.php'; ?>
+<?php require_once 'components/navbar.php'; ?>
+
+<div class="container-home">
+    <div class="left-section">
+        <img src="img/CroodsChart.png" alt="Chart Image">
     </div>
-</body>
-</html>
+    <div class="right-section">
+        <h1 class="title">tata dompet</h1>
+        <p class="tagline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, dicta enim sunt doloremque repellat perferendis.</p>
+        <a href="index.php?page=main" class="cta-button">Let's Start</a>
+    </div>
+</div>
+
+<?php require_once 'components/footer.php'; ?>
